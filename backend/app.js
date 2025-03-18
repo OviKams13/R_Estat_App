f40465ab52ecb8e44b5e3b38d307d9decce0f3a4
Ovidiu <okambembo13@gmail.com>
2025-03-18 22:36:45 +0200

    bv8

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
-// import postRout from ".routes/post.route.js;"
+import postRoute from "./routes/post.route.js";
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(cookieParser());
 dotenv.config();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
-// app.use("/api/posts", postRout);
+app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/test", testRoute);
