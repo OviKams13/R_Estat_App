7ccb126e0ded6264a1ff4185745193e63e96b6e7
Ovidiu <okambembo13@gmail.com>
2025-03-22 00:36:06 +0200

    bv10

--- frontend/src/routes/listPage/listPage.jsx
+++ frontend/src/routes/listPage/listPage.jsx
@@ -1,11 +1,11 @@
-import { listData } from "../../lib/dummydata";
 import "./listPage.scss";
 import Filter from "../../components/filter/Filter"
 import Card from "../../components/card/Card"
 import Map from "../../components/map/Map";
+import { useLoaderData } from "react-router-dom";
 
 function ListPage() {
-  const data = listData;
+  const data = useLoaderData();
 
   return <div className="listPage">
     <div className="listContainer">
