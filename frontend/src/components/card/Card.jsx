db2fef62355c859cbfa814b4469c4e1c8e0e0a27
Ovidiu <okambembo13@gmail.com>
2025-03-23 21:27:43 +0200

    bv12

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -5,7 +5,7 @@ function Card({ item }) {
   return (
     <div className="card">
       <Link to={`/${item.id}`} className="imageContainer">
-      <img src={item.images && item.images.length > 0 ? item.images[0] : "/noimage.jpg"} alt="" />
+      <img src={item.images[0]} alt="" />
       </Link>
       <div className="textContainer">
         <h2 className="title">
