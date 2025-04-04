ebb05485316b184436288b20c8e34d06dc99dfd1
OviKams13 <okambembo13@gmail.com>
2025-04-04 20:38:29 +0300

    Update verifyToken.js

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) => {
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-  jwt.verify(token, "cahihYzn1KyQV/BS+vkGwZbeuudVWf4A2GDgjFR7t7A=", async (err, payload) => {
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
     if (err) return res.status(403).json({ message: "Token is not Valid!" });
     req.userId = payload.id;
 
