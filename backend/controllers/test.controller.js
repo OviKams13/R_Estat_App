ccebeed6445f8794c85dcc5bdcc8784d81e2e862
OviKams13 <okambembo13@gmail.com>
2025-04-04 20:38:02 +0300

    Update test.controller.js

--- backend/controllers/test.controller.js
+++ backend/controllers/test.controller.js
@@ -10,7 +10,7 @@ export const shouldBeAdmin = async (req, res) => {
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-  jwt.verify(token, "cahihYzn1KyQV/BS+vkGwZbeuudVWf4A2GDgjFR7t7A=", async (err, payload) => {
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
     if (err) return res.status(403).json({ message: "Token is not Valid!" });
     if (!payload.isAdmin) {
       return res.status(403).json({ message: "Not authorized!" });
