759644cfd198a96b87c5d9c1709e463843b8eb33
OviKams13 <okambembo13@gmail.com>
2025-04-04 21:49:12 +0300

    Update apiRequest.js

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://estat-backend.onrender.com",
+  baseURL: "https://estat-backend.onrender.com/api",
   withCredentials: true,
 });
 
