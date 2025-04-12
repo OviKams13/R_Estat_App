f9012f757ce62c8a8674f1e896a2b9d05963030c
Ovidiu <okambembo13@gmail.com>
2025-04-12 22:53:33 +0300

    fix sessions

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user")) || null
+    JSON.parse(sessionStorage.getItem("user")) || null
   );
 
   const updateUser = (data) => {
@@ -12,7 +12,11 @@ export const AuthContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(currentUser));
+    if (currentUser) {
+      sessionStorage.setItem("user", JSON.stringify(currentUser));
+    } else {
+      sessionStorage.removeItem("user");
+    }
   }, [currentUser]);
 
   return (
