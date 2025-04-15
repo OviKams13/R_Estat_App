3e91610b2ac5a3183899e1c722d940458479b5ac
OviKams13 <okambembo13@gmail.com>
2025-04-15 21:30:01 +0300

    Update List.jsx

--- frontend/src/components/list/List.jsx
+++ frontend/src/components/list/List.jsx
@@ -1,14 +1,31 @@
-import './list.scss'
-import Card from"../card/Card"
+import "./list.scss";
+import Card from "../card/Card";
+import { useState, useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
 
-function List({posts}){
+function List({ posts }) {
+  const { currentUser } = useContext(AuthContext);
+  const [chat, setChat] = useState(null);
+
+  const handleOpenChat = async (receiverId) => {
+    try {
+      const res = await apiRequest.post("/chats", { receiverId });
+      setChat(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className='list'>
-      {posts.map(item=>(
-        <Card key={item.id} item={item}/>
+    <div className="list">
+      {posts.map((item) => (
+        <Card key={item.id} item={item} handleOpenChat={handleOpenChat} from="profile" />
+
       ))}
+
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
